05243ad948f433c41a625c981b5f675473750c9a
Migulinux98 <migulinux@gmail.com>
2021-12-09 16:21:59 +0100

    The fisrt version

---
+++ random_num.cs
@@ -0,0 +1,19 @@
+using System;
+ 
+namespace test
+{
+    class Program
+    {
+        public static void Main ()
+        {
+ 
+            Random r= new Random ();
+ 
+            
+            Console.WriteLine(r.Next (10,101));
+ 
+           
+        }
+ 
+    }
+}
